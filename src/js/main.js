b38e5424087b72adefa87340ccc5877f1e84b5ad
NektoStas <nektostas007@gmail.com>
2023-04-23 00:20:32 +0200

    card swiper_hero

--- src/js/main.js
+++ src/js/main.js
@@ -226,13 +226,13 @@ const slides = document.querySelectorAll('.slide');
 for (const slide of slides){
     slide.addEventListener('click', () => {
         clearActiveClasses();
-        slide.classList.add('active');
+        slide.classList.add('active_hero');
     })
 }
 
 function clearActiveClasses(){
     slides.forEach((slide)=>{
-        slide.classList.remove('active')
+        slide.classList.remove('active_hero')
     })
 }
 
